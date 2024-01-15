7f639f5dbdbaeffe7b75d5b13e33da6c487f31fe
Giovani Chaves <giovani_chaves@hotmail.com>
2024-01-15 20:19:36 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,11 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+    }
+}
+
+// Identação
+// ctrl + k + d 
